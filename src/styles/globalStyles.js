5d506b54d9ee05376710e0618358dab7eeacab1d
Amiinoz <momagan@gmail.com>
2020-05-16 22:45:43 -0600

    Adding custom cursor

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -57,17 +57,17 @@ export const Flex = styled.div`
 
 // Custtome Cursor
 export const Cursor = styled.div`
-position: absolute;
-top: 0;
-left: 0;
-width: 32px;
-height: 32px;
-backgound: ${(props) => props.theme.red};
-border-radius: 100%;
-transform: translate(-50%, -50%);
-transition: all 0.1s ease-in-out;
-transition-property: width, height, border;
-will-change: : width, height, transform, border;
-pointer-events: none;
-z-index: 999;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 32px;
+  height: 32px;
+  background: #ea281e;
+  border-radius: 100%;
+  transform: translate(-50%, -50%);
+  transition: all 0.1s ease-out;
+  transition-property: width, height, border;
+  will-change: width, height, transform, border;
+  pointer-events: none;
+  z-index: 999;
 `;
